34a3124b38173a3f92befd9499f909e49ddbbdd3
Matias Eyzaguirre <matias@fortlogic.net>
2017-09-22 00:54:19 -0600

    Initial Commit

---
+++ FortlogicFoundation/FortlogicFoundation.h
@@ -0,0 +1,19 @@
+//
+//  FortlogicFoundation.h
+//  FortlogicFoundation
+//
+//  Created by Matias Eyzaguirre on 9/22/17.
+//  Copyright © 2017 Matias Eyzaguirre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FortlogicFoundation.
+FOUNDATION_EXPORT double FortlogicFoundationVersionNumber;
+
+//! Project version string for FortlogicFoundation.
+FOUNDATION_EXPORT const unsigned char FortlogicFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FortlogicFoundation/PublicHeader.h>
+
+
