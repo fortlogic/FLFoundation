d9e11fdc231ce47e4fca64d0526075d5bb976520
Matias Eyzaguirre <matias@fortlogic.net>
2017-10-16 17:00:24 -0600

    Added framework template.

---
+++ FLAppKit/FLAppKit.h
@@ -0,0 +1,19 @@
+//
+//  FLAppKit.h
+//  FLAppKit
+//
+//  Created by Matias Eyzaguirre on 2017-10-16.
+//  Copyright © 2017 Matias Eyzaguirre. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FLAppKit.
+FOUNDATION_EXPORT double FLAppKitVersionNumber;
+
+//! Project version string for FLAppKit.
+FOUNDATION_EXPORT const unsigned char FLAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLAppKit/PublicHeader.h>
+
+
